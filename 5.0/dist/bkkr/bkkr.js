e28ff3881789656b5ab7a6e1792b235567feebe4
Imre Bekker <i.bekker@ingenia-it.de>
2021-01-28 15:18:44 +0100

    chore(): Docs update

--- 5.0/dist/bkkr/bkkr.js
+++ 5.0/dist/bkkr/bkkr.js
@@ -116,7 +116,7 @@ var __assign=this&&this.__assign||function(){return(__assign=Object.assign||func
 
   var resourcesUrl = scriptElm ? scriptElm.getAttribute('data-resources-url') || scriptElm.src : '';
   var start = function() {
-    var url = new URL('./p-9fa307e8.system.js', new URL(resourcesUrl, window.location.origin));
+    var url = new URL('./p-75f8dd1b.system.js', new URL(resourcesUrl, window.location.origin));
     System.import(url.href);
   };
 

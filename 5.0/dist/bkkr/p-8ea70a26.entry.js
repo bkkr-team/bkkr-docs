919a01ab61263dd2f2ee9ed3837c31ac91493221
Imre Bekker <i.bekker@ingenia-it.de>
2021-05-03 16:34:57 +0200

    docs(): Update

---
+++ 5.0/dist/bkkr/p-8ea70a26.entry.js
@@ -0,0 +1,1 @@
+import{r as t,c as r}from"./p-856de026.js";const e=class{constructor(e){t(this,e),this.bkkrRouteRedirectChanged=r(this,"bkkrRouteRedirectChanged",7)}propDidChange(){this.bkkrRouteRedirectChanged.emit()}connectedCallback(){this.bkkrRouteRedirectChanged.emit()}static get watchers(){return{from:["propDidChange"],to:["propDidChange"]}}};export{e as bkkr_route_redirect}

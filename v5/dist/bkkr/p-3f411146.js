7d2dc2d6c920e34082052c81342b1ed4493711c8
Imre Bekker <i.bekker@ingenia-it.de>
2022-02-01 09:15:05 +0100

    docs(): update

---
+++ v5/dist/bkkr/p-3f411146.js
@@ -0,0 +1,4 @@
+/*!
+ * (C) BKKR Framework https://bkkr-team.github.io/bkkr-docs/ - MIT License
+ */
+const o=o=>o&&""!==o.dir?"rtl"===o.dir.toLowerCase():"rtl"===(null===document||void 0===document?void 0:document.dir.toLowerCase());export{o as i}
---
+++ v5/dist/bkkr/p-d0fdf8c8.entry.js
@@ -0,0 +1,4 @@
+/*!
+ * (C) BKKR Framework https://bkkr-team.github.io/bkkr-docs/ - MIT License
+ */
+export{S as bkkr_spinner}from"./p-cc8afa83.js";import"./p-fef964f0.js";import"./p-18201c80.js";
